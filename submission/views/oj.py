c366aa4df17db47b6151fd5bd6b1149302ce9769
binbon <binbon@gmail.com>
2020-12-25 16:54:56 +0800

    allow to modify submission result

--- submission/views/oj.py
+++ submission/views/oj.py
@@ -129,7 +129,7 @@ class SubmissionAPI(APIView):
             
             problem_id = problem.id
             user = User.objects.get(id=submission.user_id)
-            user_profile = user.userprofile
+            profile = user.userprofile
             if problem.rule_type == ProblemRuleType.ACM:
                 acm_problems_status = profile.acm_problems_status.get("problems", {})
                 if acm_problems_status[problem_id]["status"] != JudgeStatus.ACCEPTED:
