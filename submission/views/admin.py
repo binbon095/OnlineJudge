dba8f5f95df605e95e0e6ea92417225c3d31262b
binbon <binbon@gmail.com>
2020-12-31 20:44:15 +0800

    manual judge

--- submission/views/admin.py
+++ submission/views/admin.py
@@ -22,3 +22,20 @@ class SubmissionRejudgeAPI(APIView):
 
         judge_task.send(submission.id, submission.problem.id, updated_result)
         return self.success()
+
+    @super_admin_required
+    def put(self, request):
+        id = request.GET.get("id")
+        if not id:
+            return self.error("Parameter error, id is required")
+        try:
+            submission = Submission.objects.select_related("problem").get(id=id) #, contest_id__isnull=True)
+        except Submission.DoesNotExist:
+            return self.error("Submission does not exists")
+        submission.statistic_info = {}
+        submission.save()
+
+        updated_result = int(request.GET.get("result"))
+
+        judge_task.send(submission.id, submission.problem.id, updated_result)
+        return self.success()
